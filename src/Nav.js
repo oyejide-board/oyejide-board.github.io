f4a228b82c8e2c12b48b960158ad5e62e8fa9a2d
oyejide-board <oyemoyin26@gmail.com>
2022-09-24 01:54:54 +0100

    2nd day

---
+++ src/Nav.js
@@ -0,0 +1,21 @@
+import {useState,useEffect} from 'react';
+
+export default function Nav () {
+    
+
+  const [items, setItems] = useState(["the stiuff"]);
+
+  const FetchAPi = () => {
+
+  }
+  
+
+    return (
+      
+      <div><h1>{items}</h1>
+      <p>theis</p>
+       <button></button></div>
+
+      
+    );
+    }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Nav from './Nav';
 import reportWebVitals from './reportWebVitals';
+import  Component from './Comp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Nav />
+    <Component/>
   </React.StrictMode>
 );
 
